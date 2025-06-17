5c19d2932f969cfc917d6bbc47d780f5237047f1
vnk pshk <vnkpshk@gmail.com>
2025-06-17 21:19:03 +0300

    task1

---
+++ src/сircle.java
@@ -0,0 +1,11 @@
+class circle {
+    //main метод
+    // Комбинации: main, psvm
+    public static void main(String[] args) {
+
+        //(условие)?(true):(false);
+        System.out.println(1 == 1 ? "Yes" : "No");
+
+        //CTRL+ALT+L - форматирование кода JavaCodeConvention
+    }
+}
