505b874bb2865b04949fff837c3d7b1f7c3baa4e
vnk pshk <vnkpshk@gmail.com>
2025-06-17 21:41:08 +0300

    task2

---
+++ src/circle1.java
@@ -0,0 +1,10 @@
+class circle1 {
+    //main метод
+    // Комбинации: main, psvm
+    public static void main(String[] args) {
+
+        int age = 110; //Переменная(Характеристика)
+
+        System.out.println("fdvdfbsw");
+    }
+}
